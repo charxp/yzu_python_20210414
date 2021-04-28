f0003693e151ab5964b1c3cd8e24fa3ece82ff21
Charlie <charxp.nn@gmail.com>
2021-04-28 18:56:33 +0800

    0421

--- day02/hello4.py
+++ day02/hello4.py
@@ -1,22 +1,21 @@
 # -*- coding:UTF-8 -*-
-
-account = 10000000
-stock = '台積電'
-price = 590.5
-amount = 5432
+account = 10000000  # 帳戶資金
+stock = '台積電'  # 標的
+price = 590.5  # 成交價
+amount = 5432  # 股數
 tax_ratio = 1.425/1000
 fee_ratio = 3/1000
-
+# 買入成本
 cost = price * amount * (1+tax_ratio)
+# 帳戶剩餘資金
 account = account - cost
-
 print(cost, account)
-print("成本", cost) #預設 sep=" " 預設 end= " \n"
+print("成本", cost)  # 預設 sep=" " 預設 end="\n"
 print("成本", cost, sep="=", end=".\n")
-
+# %f (浮點數) %d (整數) %s (字串)
 print("成本: %.1f" % cost)
-#買進 台積電 5432 股 花費成本 ＄ 3212166.8 帳戶餘額 $ 6787833.2
-print("買進 %s %d 股 花費成本 ＄ %.1f 帳戶餘額 ＄ %.1f" % (stock, amount, cost, account))
+# 欲印出 --> 買進 台積電 5432 股 花費成本 $ 3212166.8 帳戶餘額 $ 6787833.2
+print("買進 %s %d 股 花費成本 $ %.1f 帳戶餘額 $ %.1f" % (stock, amount, cost, account))
 print("買進 %s %d 股 花費成本 $ %.1f 帳戶餘額 $ %s" % (stock, amount, cost, format(int(account*10)/10, ',')))
 print("帳戶餘額 $ %s" % format(int(account*10)/10, ','))
 
